e6f68e8f421aa85692d0df2734e7ed1cc57aba62
Vincent Trivett <trivett@gmail.com>
2013-04-20 15:20:37 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trainingwheels::Application.config.secret_token = 'd3b751e799770685f85a7c3c22a206bac24bc8d0772a9fb551d63a273768503aea23251d10bf6b56ad5207ac7b6fafc62a018247dfe907fecd7c87ecd8b2c459'
